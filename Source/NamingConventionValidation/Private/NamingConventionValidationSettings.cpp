58d30c2446bcf26417eec959a3db51ec3423f9cc
Michael Delva <michael@emidee.net>
2020-07-09 11:39:06 +0200

    Added settings to allow renaming in developers folder and / or only the game folder

--- Source/NamingConventionValidation/Private/NamingConventionValidationSettings.cpp
+++ Source/NamingConventionValidation/Private/NamingConventionValidationSettings.cpp
@@ -2,12 +2,24 @@
 
 UNamingConventionValidationSettings::UNamingConventionValidationSettings()
 {
+    AllowRenamingInDevelopersFolder = false;
+    AllowRenamingOnlyInGameFolder = true;
     DoesValidateOnSave = true;
     BlueprintsPrefix = "BP_";
 }
 
 bool UNamingConventionValidationSettings::IsPathExcludedFromValidation( const FString & path ) const
 {
+    if ( !path.StartsWith( "/Game/" ) && AllowRenamingOnlyInGameFolder )
+    {
+        return true;
+    }
+
+    if ( path.StartsWith( "/Game/Developers/" ) && !AllowRenamingInDevelopersFolder )
+    {
+        return true;
+    }
+
     for ( const auto & excluded_path : ExcludedDirectories )
     {
         if ( path.Contains( excluded_path.Path ) )
